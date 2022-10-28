e7a611c82f91975d665c89fbaed771583210fe2c
Benjamin Lambert <blambert@artfx.fr>
2022-10-28 16:52:43 +0200

    unfixed compil

--- SortAlgorithm/SortAlgorithm/SortAlgorithm.cpp
+++ SortAlgorithm/SortAlgorithm/SortAlgorithm.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
-#include <bitset>
+
 
 using namespace std;
 
@@ -9,11 +9,91 @@ using Index = int;
 using Nombre = int;
 using Table = vector<Nombre>;
 
+//------------------------------------------------- || TAPE EQUILIBRIUM || -------------------------------------------------------
+
+// non fonctionnel
+
+#include <math.h>
+
+int tapeEquilibrium(vector<int>& A) {
+    if (A.size() <= 1) return 0;
+    if (A.size() == 2) return static_cast<int>(static_cast<double>(std::fabs(A[0] - A[1])));
+
+    std::sort(A.begin(), A.end());
+
+    int sizeA = A.size(); int split{ 0 };
+    if (sizeA % 2 == 0)
+    {
+        split = (sizeA / 2);
+    }
+    else split = (sizeA / 2) + 1;
+
+    int sumLeft{ 0 };
+    auto ite = A.begin();
+
+    while (ite - A.begin() < split)
+    {
+        sumLeft += *ite;
+        ite++;
+    }
+
+    int sumRight{ 0 };
+
+    while (ite < A.end())
+    {
+        sumRight += *ite;
+        ite++;
+    }
+
+    ite -= split;
+    int pivVal = sumRight - sumLeft; int minRes{ pivVal };
+    int tempVal{ pivVal };
 
 
+    while (ite < A.end())
+    {
+        if (minRes > tempVal) minRes = tempVal;
 
-//-----------------------------------------------------------------------------------------------
+        sumRight -= *ite;
+        sumLeft += *ite;
+        tempVal = sumLeft - sumRight;
+        tempVal = static_cast<int>(std::fabs(static_cast<double>(tempVal)));
+        ite++;
+    }
 
+    return minRes;
+}
+
+
+//---------------------------------------------- || PERM MISSING ELEMENT || --------------------------------------------------------
+
+
+#include <algorithm>
+
+int permMissingElem(vector<int>& A) {
+
+    if (A.size() < 1) return 1;
+    std::sort(A.begin(), A.end());
+
+    auto ite1 = A.begin(); auto ite2 = A.begin();
+
+    if (A[0] != 1) return 1;
+    else
+    {
+        while (ite1 < A.end())
+        {
+            ite2++;
+            if (*ite1 == (*ite2) - 1) ite1++;
+            else return (*ite1) + 1;
+        }
+
+        return 1;
+    }
+}
+
+
+//---------------------------------------------------------- || FROG JUMP || ----------------------------------------------------------
+// 100%
 
 #include <cmath>
 int frogJumps(int X, int Y, int D) {
@@ -35,8 +115,8 @@ int frogJumps(int X, int Y, int D) {
 }
 
 
-//------------------------------------------------------------------------------------------------
-
+//----------------------------------------------------- || DETECT UNAPAIRED INT || -------------------------------------------------------
+//100%
 
 #include <algorithm>
 int detectUnpairedInt(vector<int>& A) {
@@ -63,8 +143,8 @@ int detectUnpairedInt(vector<int>& A) {
 
 
 
-//------------------------------------------------------------------------------------------------
-
+//--------------------------------------------- || ROTAT ARRAY || ---------------------------------------------------
+// 66%
 
 vector<int> rotateArray(vector<int>& A, int K) {
 
@@ -124,9 +204,10 @@ vector<int> rotateArray(vector<int>& A, int K) {
 }
 
 
-//-----------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------- || BIT GAP || -------------------------------------------------------------
+//100%
 
-
+#include <bitset>
 int bitGap(int N) {
 
     const int size = 24;
@@ -158,8 +239,8 @@ int bitGap(int N) {
 }
 
 
-//--------------------------------------------------------------------------------------------------------------
-
+//---------------------------------------------------|| NON OPTI REC QUICK SORT || -----------------------------------------------------------
+// Crash above a few dozens of elements i think!
 
 Table recQuickSort(Table& myTable,Index indexLeft, Index indexPiv )
 {
